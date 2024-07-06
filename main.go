80d651970c3722a424579b5935dfc62756550296
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-06 11:14:16 -0300

    feat: improved perf

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math"
 	"os"
 	"runtime/pprof"
 	"sort"
@@ -15,25 +14,29 @@ import (
 )
 
 type Location struct {
-	min   float32
-	max   float32
-	mean  float32
-	count int
+	min   int16
+	max   int16
+	sum   int64
+	count uint32
 }
 
 func NewLocation() *Location {
 	return &Location{
-		min:   999999,
-		max:   -999999,
-		mean:  0,
+		min:   9999,
+		max:   -9999,
+		sum:   0,
 		count: 0,
 	}
 }
 
-func (loc *Location) Add(temp float32) {
-	loc.min = float32(math.Min(float64(loc.min), float64(temp)))
-	loc.max = float32(math.Max(float64(loc.max), float64(temp)))
-	loc.mean = (loc.mean*float32(loc.count) + float32(temp)) / (float32(loc.count) + 1)
+func (loc *Location) Add(temp int16) {
+	if temp < loc.min {
+		loc.min = temp
+	}
+	if temp > loc.max {
+		loc.max = temp
+	}
+	loc.sum += int64(temp)
 	loc.count += 1
 }
 
@@ -66,18 +69,25 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(workers)
 
+	var chunk []byte
+	var lines [][]byte
+
 	go func() {
 		for {
 			n, err := reader.Read(buf)
 			if err != nil {
 				if err == io.EOF {
+					// last chunk
+					if len(leftData) > 0 {
+						linesChan <- [][]byte{leftData}
+					}
 					break
 				}
 				panic(err)
 			}
 
-			chunk := append(leftData, buf[:n]...)
-			lines := bytes.Split(chunk, []byte{'\n'})
+			chunk = append(leftData, buf[:n]...)
+			lines = bytes.Split(chunk, []byte{'\n'})
 			lines, leftData = lines[:len(lines)-1], lines[len(lines)-1]
 			linesChan <- lines
 		}
@@ -115,26 +125,27 @@ func main() {
 
 	for _, name := range keys {
 		loc := m[name]
-		fmt.Printf("%s: %.1f/%.1f/%.1f\n", name, loc.min, loc.mean, loc.max)
+		mean := float32(loc.sum) / float32(loc.count) / 10
+		fmt.Printf("%s: %.1f/%.1f/%.1f\n", name, float32(loc.min)/10, mean, float32(loc.max)/10)
 	}
 }
 
-func parse(b []byte) float32 {
-	var v int32
-	var isNeg int32 = 1
+func parse(b []byte) int16 {
+	var v int16
+	var isNeg int16 = 1
 
 	for i := 0; i < len(b)-1; i++ {
 		char := b[i]
 		if char == '-' {
 			isNeg = -1
 		} else if char == '.' {
-			digit := int32(b[i+1] - '0')
+			digit := int16(b[i+1] - '0')
 			v = v*10 + digit
 		} else {
-			digit := int32(char - '0')
+			digit := int16(char - '0')
 			v = v*10 + digit
 		}
 	}
 
-	return float32(v*isNeg) / 10
+	return v * isNeg
 }
